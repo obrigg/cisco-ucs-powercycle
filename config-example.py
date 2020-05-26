6614613f7c3cff00b075dc473fe1b377ece9e589
Oren Brigg <obrigg@cisco.com>
2020-05-26 15:02:48 +0300

    Aligned config-examples

--- config-example.py
+++ config-example.py
@@ -1,10 +1,5 @@
 servers = [
-    {"name": "server-1",
-    "ip": "192.168.1.2",
-    "user": "admin",
-    "password":"password"},
-    {"name": "server-2",
-    "ip": "192.168.1.3",
-    "user": "admin",
-    "password": "password"}
+    {"name": "server-1", "ip": "192.168.1.2", "user": "user", "password": "password"},
+    {"name": "server-2", "ip": "192.168.1.3", "user": "user", "password": "password"},
+    {"name": "server-3", "ip": "192.168.1.4", "user": "user", "password": "password"}
     ]
